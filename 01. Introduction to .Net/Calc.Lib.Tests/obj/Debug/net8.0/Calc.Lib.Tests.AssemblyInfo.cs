cffd337981b559787e7a5364c2f66ed3ee5347e5
Mikhail Fedotov <mihanfedotov@yandex.ru>
2025-09-20 15:00:43 +0300

    Update

--- 01. Introduction to .Net/Calc.Lib.Tests/obj/Debug/net8.0/Calc.Lib.Tests.AssemblyInfo.cs
+++ 01. Introduction to .Net/Calc.Lib.Tests/obj/Debug/net8.0/Calc.Lib.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calc.Lib.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c19a91b89b0874e2ed57e00bc9e9756f7e5f441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2790554ed99964236086442b0bcf48528b3f6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calc.Lib.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calc.Lib.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
